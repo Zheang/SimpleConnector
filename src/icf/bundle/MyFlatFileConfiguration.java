b021dda0fe00c08f1c819ee227301996f4ff7d53
Zheang <Zheang@users.noreply.github.com>
2015-04-07 09:51:33 -0700

    Update MyFlatFileConfiguration.java

--- src/icf/bundle/MyFlatFileConfiguration.java
+++ src/icf/bundle/MyFlatFileConfiguration.java
@@ -1,1 +1,40 @@
 //Created by Zheang
+package icf.bundle;
+
+import java.io.File;
+
+import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.identityconnectors.framework.spi.AbstractConfiguration;
+import org.identityconnectors.framework.spi.ConfigurationProperty;
+
+public class MyFlatFileConfiguration extends AbstractConfiguration {
+	private String fileName = "";
+	private String delimiter = ",";
+
+	@Override
+	public void validate() {
+		System.out.println("Validate");
+		File f = new File(fileName);
+		if(!f.isFile())
+			throw new ConnectorException("File not exist");
+	}
+
+	@ConfigurationProperty(order = 1, helpMessageKey = "USER_ACCOUNT_STORE_HELP", displayMessageKey = "USER_ACCOUNT_STORE_DISPLAY")
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	@ConfigurationProperty(order = 2, helpMessageKey = "USER_STORE_TEXT_DELIM_HELP", displayMessageKey = "USER_STORE_TEXT_DELIM_DISPLAY")
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	public String getFileName() {
+		return this.fileName;
+	}
+
+	public String getDelimiter() {
+		return this.delimiter;
+	}
+
+}
